55106493f30ba9e973d599d72e40432d2c06a9ad
Mihindukulasuriya Shonal Nirash Fernando <129303512+ShonalFernando@users.noreply.github.com>
2025-04-17 23:37:48 +0530

    Major CHanges

---
+++ BITChecker/Model/GPA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BITChecker.Model
+{
+    public class GPA
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime DateCalculated { get; set; } = DateTime.Now;
+
+        // One GPA to many scores
+        public List<PerSubjectScore> SubjectScores { get; set; } = new();
+
+        [Required]
+        public decimal GPACalculated { get; set; }
+
+        [Required]
+        public string Status { get; set; } = "Calculating...";
+    }
+}
---
+++ BITChecker/ViewModel/DataFields/GPACalculatorViewModel.cs
@@ -0,0 +1,60 @@
+﻿using BITChecker.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BITChecker.ViewModel
+{
+    public partial class GPACalculatorViewModel : ViewModelBase
+    {
+        public ObservableCollection<int> AvailableYears { get; set; } = new();
+        public ObservableCollection<Subject> AvailableSubjects { get; set; } = new();
+        public ObservableCollection<string> AvailableGrades { get; set; } = new();
+        public ObservableCollection<PerSubjectScore> SelectedScores { get; set; } = new();
+
+        private int _selectedYear;
+        public int SelectedYear
+        {
+            get => _selectedYear;
+            set
+            {
+                if (_selectedYear != value)
+                {
+                    _selectedYear = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private Subject? _selectedSubject;
+        public Subject? SelectedSubject
+        {
+            get => _selectedSubject;
+            set
+            {
+                if (_selectedSubject != value)
+                {
+                    _selectedSubject = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string? _selectedGrade;
+        public string? SelectedGrade
+        {
+            get => _selectedGrade;
+            set
+            {
+                if (_selectedGrade != value)
+                {
+                    _selectedGrade = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
