61be8ade482517004b5e7a9091ab94877fd81842
Mihindukulasuriya Shonal Nirash Fernando <129303512+ShonalFernando@users.noreply.github.com>
2025-04-20 23:32:16 +0530

    Major Changes

--- BITChecker/obj/Debug/net8.0-windows/View/ShellWindow.g.i.cs
+++ BITChecker/obj/Debug/net8.0-windows/View/ShellWindow.g.i.cs
@@ -65,7 +65,7 @@ namespace BITChecker.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/BITChecker;V1.0.0.0;component/view/shellwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BITChecker;component/view/shellwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\ShellWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
